a246f2c16673379745323d0a0b0f3acc64d0da12
Roman Pykhnivskyi <pro1710@gmail.com>
2022-08-31 13:26:31 +0300

    Update 63_unique_paths_2.cpp

--- 63_unique_paths_2.cpp
+++ 63_unique_paths_2.cpp
@@ -3,16 +3,11 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
         vector<vector<unsigned>> ans(obstacleGrid.size(), vector<unsigned>(obstacleGrid.front().size(), 0));
-        for (int i = 0 ; i < ans.size(); ++i) {
-            for (int j = 0; j < ans[i].size(); ++j) {
-                ans[i][j] = obstacleGrid[i][j];
-            }
-        } 
         
         for (int i = ans.size()-1; i >= 0; --i) {
             for (int j = ans[i].size()-1; j >= 0; --j) {
                 
-                if (ans[i][j] == 1) {
+                if (obstacleGrid[i][j] == 1) {
                     ans[i][j] = 0;
                     continue;
                 }
