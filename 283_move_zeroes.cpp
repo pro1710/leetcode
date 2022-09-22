576d8ab920479912a8be1fb1ae069225195bf939
Roman Pykhnivskyi <pro1710@gmail.com>
2022-09-22 13:18:33 +0300

    Update 283_move_zeroes.cpp

--- 283_move_zeroes.cpp
+++ 283_move_zeroes.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (int i = 0, e = nums.size(); i < e; ++i) {
-            if (nums[i] != 0) {
-                continue;
-            }
-            for (int j = i+1; j < nums.size(); j++) {
-                if (nums[j] != 0) {
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                    break;
-                }
+        for (int nz = 0, cur = 0; cur < nums.size(); cur++) {
+            if (nums[cur] != 0) {
+                swap(nums[nz++], nums[cur]);
             }
         }
     }
