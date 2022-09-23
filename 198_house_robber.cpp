f4d2303b4f607adcbc313f602ee6c070e81869e4
Roman Pykhnivskyi <pro1710@gmail.com>
2022-09-23 13:09:24 +0300

    Create 198_house_robber.cpp

---
+++ 198_house_robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        
+        nums[nums.size()-2] = max(nums[nums.size()-2], nums[nums.size()-1]);
+        
+        for (int i = nums.size()-3; i >= 0; --i) {
+            nums[i] = max(nums[i+1], nums[i+2] + nums[i]);
+        }
+        
+        return max(nums[0], nums[1]);
+    }
+};
