16f2503fad81861d2ed99d73f577677510fac66d
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-02 22:02:36 +0300

    Create 119_pascals_triangle_2.cpp

---
+++ 119_pascals_triangle_2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    
+public:
+    vector<int> getRow(int rowIndex) {
+            
+        vector<int> res = {1};
+        for (int i  = 0; i < rowIndex; ++i) {
+            res = nexRow(res);
+        }
+        return res;
+    }
+
+    vector<int> nexRow(const vector<int>& prev) {
+        vector<int> next(prev.size()+1, 1);
+        for (int i = 1; i < next.size()-1; ++i) {
+            next[i] = prev[i-1] + prev[i];
+        }
+        return move(next);
+    }
+};
