56000e477071aa306d9f3f94deb08a3401c9cb53
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-07 15:52:02 +0300

    Update 5_longest_palindromic_substring.cpp

--- 5_longest_palindromic_substring.cpp
+++ 5_longest_palindromic_substring.cpp
@@ -30,3 +30,34 @@ public:
         return s.substr(mi, max_poly);
     }
 };
+//  //dp
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+//         int mlen = 1;
+//         int ans_i = 0;
+//         for (int i = 0; i < s.size(); ++i) {
+//             dp[i][i] = 1;
+//             if (i+1< s.size() && s[i] == s[i+1]) {
+//                 dp[i][i+1] = 1;
+//                 ans_i = i;
+//                 mlen = 2;
+//             }
+//         } 
+
+//         for (int k = 3; k <= s.size(); ++k) {
+//             for (int i = 0; i < s.size() - k + 1; ++i) {
+//                 int j = i+k-1;
+//                 if (dp[i+1][j-1] && s[i] == s[j]) {
+//                     dp[i][j] = 1;
+//                     if (k > mlen) {
+//                         ans_i = i;
+//                         mlen = k;
+//                     }
+//                 }
+//             }
+//         }
+//         return s.substr(ans_i, mlen);
+//     }   
+// };
