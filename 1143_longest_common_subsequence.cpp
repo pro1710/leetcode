6794f0363d4682c53c53de5011201b6c2dff6ba1
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-11 17:09:59 +0300

    Create 1143_longest_common_subsequence.cpp

---
+++ 1143_longest_common_subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    map<int, int> memo;
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int l1 = text1.size(), l2 = text2.size();
+        vector<vector<int>> dp(l1+1, vector<int>(l2+1));
+        for (int i = 1; i <= l1; ++i) {
+            for (int j = 1; j <= l2; ++j) {
+                if (text1[i-1] == text2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+};
