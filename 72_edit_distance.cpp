fe52f6dd2f1f15e10210a3c84944183ce49921bf
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-12 22:57:05 +0300

    Update 72_edit_distance.cpp

--- 72_edit_distance.cpp
+++ 72_edit_distance.cpp
@@ -1,24 +1,23 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();
-    const int n = word2.length();
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+public:
+    int minDistance(string word1, string word2) {
+        
+        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, 0));
+        for (int i = 0; i < dp.size(); ++i) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i < dp[0].size(); ++i) {
+            dp[0][i] = i;
+        }
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])
-          dp[i][j] = dp[i - 1][j - 1];
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-
-    return dp[m][n];
-  }
+        for (int i = 1; i < dp.size(); ++i) {
+            for (int j = 1; j < dp[0].size(); ++j) {
+                dp[i][j] = dp[i-1][j-1];
+                if (word1[i-1] != word2[j-1]) {
+                    dp[i][j] = min(dp[i][j], min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+            }
+        }
+        return dp.back().back();
+    }
 };
