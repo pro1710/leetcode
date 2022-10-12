48baa946fa702cefffb3d9df20d8926787b473ea
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-12 23:10:37 +0300

    Update 322_coin_change.cpp

--- 322_coin_change.cpp
+++ 322_coin_change.cpp
@@ -1,9 +1,7 @@
 class Solution {
-    unordered_map<int, int> memo;
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int max = amount + 1;
-        vector<int> dp(amount + 1, max);
+        vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.size(); j++) {
