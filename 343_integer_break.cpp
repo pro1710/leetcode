3769e99653b6b2f21c0013d4370f5de1c2fe978a
Roman Pykhnivskyi <pro1710@gmail.com>
2022-10-13 12:36:10 +0300

    Update 343_integer_break.cpp

--- 343_integer_break.cpp
+++ 343_integer_break.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-       if (n == 2 || n == 3) 
+        if (n == 2 || n == 3) {
             return n - 1;
-        int res = 1;
+        } 
+        int ans = 1;
         while (n > 4) {
-            res *= 3;
+            ans *= 3;
             n -= 3;
         }
-        return res * n;
-     
+        cout << n << endl;
+        return ans*n;
     }
 };
