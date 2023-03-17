1b5984b4e96d9b62628bca5c289b1991758957b2
Roman Pykhnivskyi <pro1710@gmail.com>
2023-03-17 17:20:21 +0200

    Create 157_read_n_characters_given_read4.cpp

---
+++ 157_read_n_characters_given_read4.cpp
@@ -0,0 +1,27 @@
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+
+        int res = 0;
+        int r = 0;
+        while (res < n) {
+
+            r = read4(buf+res);
+            res += r;
+            if (r < 4) {
+                break;
+            }
+        }
+        return min(n, res);
+    }
+};
